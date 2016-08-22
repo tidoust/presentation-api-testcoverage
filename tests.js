a3c37a9861ed2fe1fbae9a42e84a3d309696e049
François Daoust <fd@tidoust.net>
2016-08-22 17:04:21 +0200

    Linked existing tests with spec sections
    No rough evaluation of the coverage yet, although I reviewed the existing tests
    and filled out a few issues on the web-platform-tests repository accordingly.

---
+++ tests.js
@@ -0,0 +1,75 @@
+var tests = [
+  {
+    url: 'controlling-ua/PresentationConnection_onclosed-manual.html',
+    checks: [
+      '#closing-a-presentationconnection',
+      '#event-handlers-1'
+    ]
+  },
+  {
+    url: 'controlling-ua/PresentationConnection_onconnected-manual.html',
+    checks: [
+      '#starting-a-presentation',
+      '#establishing-a-presentation-connection',
+      '#event-handlers-1'
+    ]
+  },
+  {
+    url: 'controlling-ua/PresentationConnection_onterminated-manual.html',
+    checks: [
+      '#terminating-a-presentation-in-a-controlling-browsing-context',
+      '#event-handlers-1'
+    ],
+    comments: 'Test invalid, see: https://github.com/w3c/web-platform-tests/issues/3522'
+  },
+  {
+    url: 'controlling-ua/PresentationRequest_error.html',
+    checks: '#constructing-a-presentationrequest'
+  },
+  {
+    url: 'controlling-ua/PresentationRequest_success.html',
+    checks: '#constructing-a-presentationrequest'
+  },
+  {
+    url: 'controlling-ua/defaultRequest_success-manual.html',
+    checks: [
+      '#controlling-user-agent',
+      '#starting-a-presentation',
+      '#event-handlers'
+    ]
+  },
+  {
+    url: 'controlling-ua/getAvailability.html',
+    checks: '#getting-the-presentation-displays-availability-information'
+  },
+  {
+    url: 'controlling-ua/reconnectToPresentation_error-manual.html',
+    checks: '#reconnecting-to-a-presentation',
+    comments: [
+      'step 10',
+      'could be automated, see: https://github.com/w3c/web-platform-tests/issues/3524'
+    ]
+  },
+  {
+    url: 'controlling-ua/reconnectToPresentation_success-manual.html',
+    checks: '#reconnecting-to-a-presentation'
+  },
+  {
+    url: 'controlling-ua/startNewPresentation_error-manual.html',
+    checks: '#starting-a-presentation',
+    comments: [
+      'step 11',
+      'test invalid, see: https://github.com/w3c/web-platform-tests/issues/3527'
+    ]
+  },
+  {
+    url: 'controlling-ua/startNewPresentation_error.html',
+    checks: '#starting-a-presentation',
+    comments: 'step 1'
+  },
+  {
+    url: 'controlling-ua/startNewPresentation_success-manual.html',
+    checks: '#starting-a-presentation',
+    comments: 'Test invalid, see: https://github.com/w3c/web-platform-tests/issues/3521'
+  }
+];
---
+++ toc.js
@@ -0,0 +1,180 @@
+/**********************************************************************
+Media Source Extensions spec sections that need to be tested.
+
+The structure should be updated when needed. Follow the steps described
+in the "How to update the table" section.
+**********************************************************************/
+var toc = {
+  "name": "Table of Contents",
+  "children": [
+    {
+      "number": "6.",
+      "name": "API",
+      "url": "api",
+      "children": [
+        {
+          "number": "6.2",
+          "name": "Interface Presentation",
+          "url": "interface-presentation",
+          "children": [
+            {
+              "number": "6.2.1",
+              "name": "Controlling user agent",
+              "url": "controlling-user-agent"
+            },
+            {
+              "number": "6.2.2",
+              "name": "Receiving user agent",
+              "url": "receiving-user-agent"
+            }
+          ]
+        },
+        {
+          "number": "6.3",
+          "name": "Interface PresentationRequest",
+          "url": "interface-presentationrequest",
+          "children": [
+            {
+              "number": "6.3.1",
+              "name": "Constructing a PresentationRequest",
+              "url": "constructing-a-presentationrequest"
+            },
+            {
+              "number": "6.3.2",
+              "name": "Starting a presentation",
+              "url": "starting-a-presentation"
+            },
+            {
+              "number": "6.3.3",
+              "name": "Reconnecting to a presentation",
+              "url": "reconnecting-to-a-presentation"
+            },
+            {
+              "number": "6.3.4",
+              "name": "Event Handlers",
+              "url": "event-handlers"
+            }
+          ]
+        },
+        {
+          "number": "6.4",
+          "name": "Interface PresentationAvailability",
+          "url": "interface-presentationavailability",
+          "children": [
+            {
+              "number": "6.4.1",
+              "name": "The set of availability objects",
+              "url": "the-set-of-availability-objects"
+            },
+            {
+              "number": "6.4.2",
+              "name": "The list of available presentation displays",
+              "url": "the-list-of-available-presentation-displays"
+            },
+            {
+              "number": "6.4.3",
+              "name": "Getting the presentation displays availability information",
+              "url": "getting-the-presentation-displays-availability-information"
+            },
+            {
+              "number": "6.4.4",
+              "name": "Monitoring the list of available presentation displays",
+              "url": "monitoring-the-list-of-available-presentation-displays"
+            },
+            {
+              "number": "6.4.5",
+              "name": "Interface PresentationConnectionAvailableEvent",
+              "url": "interface-presentationconnectionavailableevent"
+            }
+          ]
+        },
+        {
+          "number": "6.5",
+          "name": "Interface PresentationConnection",
+          "url": "interface-presentationconnection",
+          "children": [
+            {
+              "number": "6.5.1",
+              "name": "Establishing a presentation connection",
+              "url": "establishing-a-presentation-connection"
+            },
+            {
+              "number": "6.5.2",
+              "name": "Sending a message through PresentationConnection",
+              "url": "sending-a-message-through-presentationconnection"
+            },
+            {
+              "number": "6.5.3",
+              "name": "Receiving a message through PresentationConnection",
+              "url": "receiving-a-message-through-presentationconnection"
+            },
+            {
+              "number": "6.5.4",
+              "name": "Interface PresentationConnectionCloseEvent",
+              "url": "interface-presentationconnectioncloseevent"
+            },
+            {
+              "number": "6.5.5",
+              "name": "Closing a PresentationConnection",
+              "url": "closing-a-presentationconnection"
+            },
+            {
+              "number": "6.5.6",
+              "name": "Terminating a presentation in a controlling browsing context",
+              "url": "terminating-a-presentation-in-a-controlling-browsing-context"
+            },
+            {
+              "number": "6.5.7",
+              "name": "Terminating a presentation in a receiving browsing context",
+              "url": "terminating-a-presentation-in-a-receiving-browsing-context"
+            },
+            {
+              "number": "6.5.8",
+              "name": "Handling a termination confirmation in a controlling user agent",
+              "url": "handling-a-termination-confirmation-in-a-controlling-user-agent"
+            },
+            {
+              "number": "6.5.9",
+              "name": "Event Handlers",
+              "url": "event-handlers-1"
+            }
+          ]
+        },
+        {
+          "number": "6.6",
+          "name": "Interface PresentationReceiver",
+          "url": "interface-presentationreceiver",
+          "children": [
+            {
+              "number": "6.6.1",
+              "name": "Creating a receiving browsing context",
+              "url": "creating-a-receiving-browsing-context"
+            }
+          ]
+        },
+        {
+          "number": "6.7",
+          "name": "Interface PresentationConnectionList",
+          "url": "interface-presentationconnectionlist",
+          "children": [
+            {
+              "number": "6.7.1",
+              "name": "Monitoring incoming presentation connections",
+              "url": "monitoring-incoming-presentation-connections"
+            },
+            {
+              "number": "6.7.2",
+              "name": "Event Handlers",
+              "url": "event-handlers-2"
+            }
+          ]
+        },
+        {
+          "number": "6.8",
+          "name": "Sandboxing and the allow-presentation keyword",
+          "url": "sandboxing-and-the-allow-presentation-keyword"
+        }
+      ]
+    }
+  ]
+};
